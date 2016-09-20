9925e41b552a142ce1cf9f467e4858785e6630d0
Travis Hegner <thegner@trilliumit.com>
2016-09-20 13:17:16 -0400

    create left and ignore it

--- example/left.go
+++ example/left.go
@@ -1,4 +1,4 @@
-package main
+package left
 
 import (
 	"github.com/travishegner/i3gobar"
@@ -6,11 +6,9 @@ import (
 
 func main() {
 	f := []func(chan<- []i3gobar.I3Block){
-		i3gobar.Uptime,
-		i3gobar.LoadAvg,
-		i3gobar.CPUGraph,
-		i3gobar.MemFree,
-		i3gobar.SwapUsed,
+		i3gobar.Date,
+		i3gobar.TimeUTC,
+		i3gobar.TimeLocal,
 	}
 
 	i3gobar.Run(f, false, 25)
