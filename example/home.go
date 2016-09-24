fdf90028ce5b067a1924d121431f6f899fa4bed6
Travis Hegner <travis.hegner@gmail.com>
2016-09-24 11:50:54 -0400

    bar for home

---
+++ example/home.go
@@ -0,0 +1,19 @@
+package home
+
+import (
+	"github.com/travishegner/i3gobar"
+)
+
+func main() {
+	f := []func(chan<- []i3gobar.I3Block){
+		i3gobar.Uptime,
+		i3gobar.LoadAvg,
+		i3gobar.CPUGraph,
+		i3gobar.MemFree,
+		i3gobar.SwapUsed,
+		i3gobar.Date,
+		i3gobar.TimeLocal,
+	}
+
+	i3gobar.Run(f, false, 25)
+}
